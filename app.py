1fae7f11ca03b9bd9a61c3edd6dfde4f450a5b0b
Wang <bo-yong.wang@hp.com>
2022-06-18 22:03:58 +0800

    third commit

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/<name>')
+def projects(name):
+    return 'Hello ' + name
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/admin')
+def admin():
+    return redirect(url_for('index'))
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("./data/data.json")
 
 if __name__ == '__main__':
     app.run(debug=True)
